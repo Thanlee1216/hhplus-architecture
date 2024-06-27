a7d6cea814ca3deeefffa58276ffb285ca1c27c0
LeeTaeHan <lth9312@gmail.com>
2024-06-28 01:30:13 +0900

    controller 단위 테스트 2차 커밋

--- src/main/java/com/study/lecture/presentation/controller/LectureController.java
+++ src/main/java/com/study/lecture/presentation/controller/LectureController.java
@@ -1,8 +1,11 @@
 package com.study.lecture.presentation.controller;
 
 import com.study.lecture.application.domain.LectureApplyDomain;
+import com.study.lecture.application.domain.LectureDomain;
+import com.study.lecture.application.domain.UserDomain;
 import com.study.lecture.application.service.LectureService;
-import com.study.lecture.presentation.dto.request.LectureRequestDTO;
+import com.study.lecture.presentation.dto.request.LectureApplyRequestDTO;
+import com.study.lecture.presentation.dto.response.LectureApplyResponseDTO;
 import com.study.lecture.presentation.dto.response.LectureResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,21 +22,26 @@ public class LectureController {
      * 특강 신청 API
      */
     @PostMapping("apply")
-    public LectureResponseDTO apply(@RequestBody LectureRequestDTO dto) {
+    public LectureApplyResponseDTO apply(@RequestBody LectureApplyRequestDTO dto) {
         LectureApplyDomain lectureApplyDomain = service.lectureApply(dto.convertToUserDomain(), dto.convertToLectureDomain());
-        return new LectureResponseDTO(lectureApplyDomain.userId(), lectureApplyDomain.userName(), lectureApplyDomain.lectureId(), lectureApplyDomain.lectureName());
+        return LectureApplyResponseDTO.of(lectureApplyDomain);
     }
 
+    /**
+     * 특강 목록 조회
+     */
     @GetMapping("")
     public List<LectureResponseDTO> getLectureList() {
-        return List.of();
+        List<LectureDomain> lectureList = service.getLectureList();
+        return LectureResponseDTO.converteFromLectureDomainToDtoList(lectureList);
     }
 
     /**
      * 특강 신청 여부 조회 API
      */
     @GetMapping("application/{userId}")
-    public boolean getLectureHistory(@PathVariable String userId) {
-        return false;
+    public List<LectureResponseDTO> getLectureApply(@PathVariable long userId) {
+        List<LectureDomain> lectureList = service.getLectureApply(new UserDomain(userId, null));
+        return LectureResponseDTO.converteFromLectureDomainToDtoList(lectureList);
     }
 }
--- src/test/java/com/study/lecture/presentation/controller/LectureControllerTest.java
+++ src/test/java/com/study/lecture/presentation/controller/LectureControllerTest.java
@@ -2,9 +2,13 @@ package com.study.lecture.presentation.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.study.lecture.application.domain.LectureApplyDomain;
+import com.study.lecture.application.domain.LectureDomain;
+import com.study.lecture.application.domain.UserDomain;
 import com.study.lecture.application.service.LectureService;
-import com.study.lecture.presentation.dto.request.LectureRequestDTO;
-import com.study.lecture.presentation.dto.response.LectureResponseDTO;
+import com.study.lecture.infrastructure.exception.ApplyOverflowException;
+import com.study.lecture.infrastructure.exception.ExceptionType;
+import com.study.lecture.presentation.dto.request.LectureApplyRequestDTO;
+import com.study.lecture.presentation.dto.response.LectureApplyResponseDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,8 +20,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -41,8 +47,9 @@ class LectureControllerTest {
         String userName = "이태한";
         long lectureId = 1L;
         String lectureName = "TDD 정말 어렵지만 할 수 있다!";
-        LectureRequestDTO requestDTO = new LectureRequestDTO(userId, userName, lectureId, lectureName);
-        LectureResponseDTO responseDTO = new LectureResponseDTO(userId, userName, lectureId, lectureName);
+        String lectureDate = "20240724";
+        LectureApplyRequestDTO requestDTO = new LectureApplyRequestDTO(userId, userName, lectureId, lectureName, lectureDate);
+        LectureApplyResponseDTO responseDTO = new LectureApplyResponseDTO(userId, userName, lectureId, lectureName);
         LectureApplyDomain applyDomain = new LectureApplyDomain(1L, userId, userName, lectureId, lectureName);
 
         when(lectureService.lectureApply(requestDTO.convertToUserDomain(), requestDTO.convertToLectureDomain())).thenReturn(applyDomain);
@@ -57,6 +64,71 @@ class LectureControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.userId").value(responseDTO.userId()))
                 .andExpect(jsonPath("$.lectureId").value(responseDTO.lectureId()));
+    }
+
+    @Test
+    @DisplayName("특강 신청에 실패하면 예외를 반환한다.")
+    void lectureApplyFail() throws Exception {
+        //given
+        long userId = 1L;
+        String userName = "이태한";
+        long lectureId = 1L;
+        String lectureName = "TDD 정말 어렵지만 할 수 있다!";
+        String lectureDate = "20240724";
+        LectureApplyRequestDTO requestDTO = new LectureApplyRequestDTO(userId, userName, lectureId, lectureName, lectureDate);
+
+        when(lectureService.lectureApply(requestDTO.convertToUserDomain(), requestDTO.convertToLectureDomain())).thenThrow(new ApplyOverflowException("ApplyFail"));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //when
+        ResultActions resultActions = mockMvc.perform(post("/lectures/apply").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(requestDTO)));
+
+        //then
+        resultActions.andDo(print())
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.message").value(ExceptionType.getMessage("ApplyFail")));
+    }
+
+    @Test
+    @DisplayName("특강의 목록을 조회한다.")
+    void getLectureListSuccess() throws Exception {
+        //given
+        List<LectureDomain> lectureList = List.of(
+                new LectureDomain(1L, "TDD 정말 어렵지만 할 수 있다!", "20240723"),
+                new LectureDomain(2L, "TDD 정말 어렵지만 할 수 있다!!!", "20240724")
+        );
+        when(lectureService.getLectureList()).thenReturn(lectureList);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(get("/lectures"));
+
+        //then
+        resultActions.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].lectureId").value(1L))
+                .andExpect(jsonPath("$[1].lectureId").value(2L));
+    }
+
+    @Test
+    @DisplayName("수강 신청에 성공한 특강의 목록을 조회한다.")
+    void getLectureApplyListSuccess() throws Exception {
+        //given
+        long userId = 1L;
+        List<LectureDomain> lectureList = List.of(
+                new LectureDomain(1L, "TDD 정말 어렵지만 할 수 있다!", "20240723"),
+                new LectureDomain(2L, "TDD 정말 어렵지만 할 수 있다!!!", "20240724")
+        );
+        when(lectureService.getLectureApply(new UserDomain(userId, null))).thenReturn(lectureList);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(get("/lectures/application/" + userId));
+
+        //then
+        resultActions.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].lectureId").value(1L))
+                .andExpect(jsonPath("$[1].lectureId").value(2L));
     }
 
 }
