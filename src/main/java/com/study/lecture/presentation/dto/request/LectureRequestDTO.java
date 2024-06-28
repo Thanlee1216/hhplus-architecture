692f2386c746830e46453d3231617f8aff971759
leetaehan <lth9312@samga.co.kr>
2024-06-28 09:01:00 +0900

    수정 1

--- src/main/java/com/study/lecture/presentation/dto/request/LectureRequestDTO.java
+++ src/main/java/com/study/lecture/presentation/dto/request/LectureRequestDTO.java
@@ -14,6 +14,6 @@ public record LectureRequestDTO(
     }
 
     public LectureDomain convertToLectureDomain() {
-        return new LectureDomain(this.lectureId, this.lectureName);
+        return new LectureDomain(this.lectureId, this.lectureName, null, 0);
     }
 }
